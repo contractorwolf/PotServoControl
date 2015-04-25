2e82232b8a604bae10a3102ef3813712fd9667b8
contractorwolf <contractorwolf@hotmail.com>
2015-04-24 23:18:37 -0400

    cleanup and comments
    might need to move this over to a new renamed project

--- PotServoControl.ino
+++ PotServoControl.ino
@@ -22,73 +22,46 @@ int reverse_horizontal_angle;
 int reverse_vertical_angle;
 
 
-
-
-
 void setup()
 {
 	//pinMode(led, OUTPUT);  
-	pinMode(pot1pin, INPUT);  
-	pinMode(pot2pin, INPUT);  
+	pinMode(pot1pin, INPUT);  // backbone servos controller
+	pinMode(pot2pin, INPUT);  // leg servo controller
 
+	Serial.begin(115200);// COM7 USB
 
-
-	//pinMode(pot1power, OUTPUT); 
-	//pinMode(pot2power, OUTPUT); 
-	//pinMode(pot2GND, OUTPUT); 
-
-
-
-
-
-	//digitalWrite(pot1power,HIGH);
-	//digitalWrite(pot2power,HIGH);
-	//digitalWrite(pot2GND,LOW);
-
-	Serial.begin(115200);
-	Serial1.begin(9600);
+	Serial1.begin(9600);// USC32 Servo Control Board
   
 
 	Serial.println("PotServoControl v1.0 Started");
 
 	delay(3000);
-  /* add setup code here */
-
 }
+
 
 void loop()
 {
 	val = analogRead(pot1pin);            // reads the value of the potentiometer (value between 0 and 1023) 
 	val2 = analogRead(pot2pin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  //0-1017 pot values
+	//0-1017 pot values
 	//530-2340 max values
 
-
-
-
-
+	// show analog values
 	Serial.print("analog1 value: ");  
 	Serial.println(val); 
+
 	Serial.print("analog2 value: ");  
 	Serial.println(val2); 
 
-
 	SendServoCommand(val, val2, time_delay);
 
-
-
-
-
-  /* add main program code here */
 	delay(100);
 }
 
 
-
-
 void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 
-
+	//first segment responding to potentiometers
 	int servo0 = map(pot_angle, 0, 1023, centers[0] + horizontal_sway, centers[0] - horizontal_sway); //horizontal_angle
 	int servo1 = map(pot_angle2,0, 1023, centers[1] + vertical_sway, centers[1] - vertical_sway); //vertical_angle
 
@@ -103,23 +76,7 @@ void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 	int servo6 = map(pot_angle, 0, 1023, centers[6] - horizontal_sway, centers[6] + horizontal_sway); //reverse_horizontal_angle
 	int servo7 = map(pot_angle2,0, 1023, centers[7] - vertical_sway, centers[7] + vertical_sway); //reverse_vertical_angle
 
-
-
-
-
-
-
-	//horizontal_angle = map(pot_angle, 0, 1023, centers[0] + horizontal_sway, centers[0] - horizontal_sway);     // scale it to use it with the servo (value between 0 and 180) 
-	//reverse_horizontal_angle = map(pot_angle, 0, 1023, centers[0] - horizontal_sway, centers[0] + horizontal_sway);     // scale it to use it with the servo (value between 0 and 180) 
-	//
-	//
-	//
-	//vertical_angle = map(pot_angle2,0, 1023, centers[1] + vertical_sway, centers[1] - vertical_sway);
-	//reverse_vertical_angle = map(pot_angle2,0, 1023, centers[1] - vertical_sway, centers[1] + vertical_sway);
-
-
-
-
+	//send to USC32 servo
 	Serial1.print("#1P");
 	Serial1.print(servo0);
 	Serial1.print("#2P");
@@ -128,8 +85,6 @@ void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 	Serial1.print(servo2);
 	Serial1.print("#4P");
 	Serial1.print(servo3);
-
-
 	Serial1.print("#5P");
 	Serial1.print(servo4);
 	Serial1.print("#6P");
@@ -138,16 +93,10 @@ void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 	Serial1.print(servo6);
 	Serial1.print("#8P");
 	Serial1.print(servo7);
-
-
-
-
-
-
 	Serial1.print("T");
 	Serial1.println(time_delay);
 
-
+	//output to USB
 	Serial.print("#1P");
 	Serial.print(servo0);
 	Serial.print("#2P");
@@ -156,8 +105,6 @@ void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 	Serial.print(servo2);
 	Serial.print("#4P");
 	Serial.print(servo3);
-
-
 	Serial.print("#5P");
 	Serial.print(servo4);
 	Serial.print("#6P");
@@ -166,17 +113,8 @@ void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 	Serial.print(servo6);
 	Serial.print("#8P");
 	Serial.print(servo7);
-
-
-
-
 	Serial.print("T");
 	Serial.println(time_delay);
-
-
-
-
-
 
 	delay(time_delay); 
 }
